23c17f5953f9cb7a4331197abefd00fef2e73b7d
yo0o0o0os <annakim04@naver.com>
2025-11-09 23:47:02 +0900

    Add files via upload

---
+++ Ex_ch10_p1.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.markdown("# 나의 소개 페이지")
+st.markdown("## 자기 소개")
+st.write("안녕하세요, 제 이름은 김윤서입니다.")
+st.write("저는 **패션**과 **HCI**에 관심있습니다.")
+st.markdown("## 좋아하는 것")
+st.write("저는 여행과 사진찍기를 좋아합니다.")
+st.markdown("가장 자주 방문하는 사이트는 [서울대 포털](https://my.snu.ac.kr/p/ST/)입니다.")
+st.markdown("## 앞으로의 목표")
+st.write("앞으로 다양한 프로젝트를 하며 실력을 키우고 싶습니다.")
